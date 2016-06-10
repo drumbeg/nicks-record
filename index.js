5c0ff383e415cedca8e8d3a0e91b2a46fe9571c5
Dave <dave@supamu.co.uk>
2016-06-10 06:47:57 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var Twitter = require('twitter');
+var request = require('request');
+var fs = require('fs');
+
+var config = fs.readFileSync('config.json');
+config = JSON.parse(config);
+
+var randomRecord;
+
+request.get({
+  uri: 'http://api.discogs.com/users/vinylmemory/collection?page=1&per_page=1000',
+  headers: {"user-agent": 'vinylmemory/0.1'}
+}, function(err, response, body) {
+  var parsedCollection = JSON.parse(body);
+  var records = parsedCollection.releases;
+  var randomRecord = records[Math.floor(Math.random() * 50)];
+
+  request.get({
+    uri: 'http://api.discogs.com/releases/' + randomRecord.release_id,
+    headers: {"user-agent": 'vinylmemory/0.1'}
+  }, function(err, response, body) {
+
+    var parsedRelease = JSON.parse(body);
+    var videoLink = parsedRelease.videos[0].uri;
+
+    var client = new Twitter({
+      consumer_key: config.twitter_auth.consumer_key,
+      consumer_secret: config.twitter_auth.consumer_secret,
+      access_token_key: config.twitter_auth.access_token_key,
+      access_token_secret: config.twitter_auth.access_token_secret
+    });
+
+    client.post('statuses/update', {
+      status:randomRecord.basic_information.artists[0].name + ' : ' + 
+      randomRecord.basic_information.title +
+      ' released in ' +
+      randomRecord.basic_information.year +
+      '. ' +
+      videoLink
+    }, function(error, tweets, response) {
+      if (!error) {
+        process.exit();
+      }
+      console.log(error);
+    });
+  });
+});
+
+
