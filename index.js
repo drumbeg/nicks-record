6d922ea9dca3c40b29a29467cb9569b6eaf2ccf8
Dave Lawton <dave@supamu.co.uk>
2016-08-02 14:39:43 +0100

    Added dupe stuff

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ config = JSON.parse(config);
 
 var randomRecord;
 var pageIncrement = 1;
+var dupeArray = [];
 
 function go() {
 request.get({
@@ -22,40 +23,47 @@ if (response.statusCode == 404) {
   var records = parsedCollection.releases;
   var randomRecord = records[Math.floor(Math.random() * records.length)];
 
-  request.get({
-    uri: 'http://api.discogs.com/releases/' + randomRecord.release_id,
-    headers: {"user-agent": 'vinylmemory/0.1'}
-  }, function(err, response, body) {
+  if (dupeArray.indexOf(randomRecord.collection_id) < 0  ) {
+    request.get({
+      uri: 'http://api.discogs.com/releases/' + randomRecord.release_id,
+      headers: {"user-agent": 'vinylmemory/0.1'}
+    }, function(err, response, body) {
 
-    var parsedRelease = JSON.parse(body);
-    var videoLink = parsedRelease.hasOwnProperty("videos") ? parsedRelease.videos[0].uri : '';
-    var discogsUrl = parsedRelease.uri;
+      var parsedRelease = JSON.parse(body);
+      var videoLink = parsedRelease.hasOwnProperty("videos") ? parsedRelease.videos[0].uri : '';
+      var discogsUrl = parsedRelease.uri;
 
-    var client = new Twitter({
-      consumer_key: config.twitter_auth.consumer_key,
-      consumer_secret: config.twitter_auth.consumer_secret,
-      access_token_key: config.twitter_auth.access_token_key,
-      access_token_secret: config.twitter_auth.access_token_secret
+      var client = new Twitter({
+        consumer_key: config.twitter_auth.consumer_key,
+        consumer_secret: config.twitter_auth.consumer_secret,
+        access_token_key: config.twitter_auth.access_token_key,
+        access_token_secret: config.twitter_auth.access_token_secret
+      });
+
+      client.post('statuses/update', {
+        status:randomRecord.basic_information.artists[0].name + ' : ' +
+        randomRecord.basic_information.title +
+        ' released in ' +
+        randomRecord.basic_information.year +
+        '. ' +
+        videoLink + ' ' + discogsUrl
+      }, function(error, tweets, response) {
+        if (error) {
+          console.log(error);
+        }
+
+        dupeArray.push(randomRecord.collection_id);
+        if (dupeArray.length >= 100) {
+          dupeArray.pop();
+        }
+      });
     });
-
-    client.post('statuses/update', {
-      status:randomRecord.basic_information.artists[0].name + ' : ' +
-      randomRecord.basic_information.title +
-      ' released in ' +
-      randomRecord.basic_information.year +
-      '. ' +
-      videoLink + ' ' + discogsUrl
-    }, function(error, tweets, response) {
-      if (error) {
-        console.log(error);
-      }
-    });
-  });
+  }
   pageIncrement++;
 }
 });
 }
 
-setInterval(go, 0.02 * 60 * 1000);
+setInterval(go, 7 * 60 * 60 * 1000);
 
 
